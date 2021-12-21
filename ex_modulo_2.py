5fb0cee9eba6b152b1720e7774b59f5bc3a4fb9f
Luciano Jesus da Silva <lucianoj75@gmail.com>
2021-12-21 20:28:24 -0300

    ex_modulo2

--- ex_modulo_2.py
+++ ex_modulo_2.py
@@ -1,5 +1,12 @@
-lista_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for nums in lista_nums:
-    print(nums)
+def ex1(lista):
+    lista_nums = lista
+    cont_pares = 0
 
+    for nums in lista_nums:
+        cont_pares += 1 if nums % 2 == 0 else 0
+
+    print('Existem {} números pares na lista {}'.format(cont_pares, lista_nums))
+
+
+ex1([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12])
