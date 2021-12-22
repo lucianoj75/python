4ac9ba1e7ed03f1c35aba0b449bf69dc986493af
Luciano Jesus da Silva <lucianoj75@gmail.com>
2021-12-21 22:06:26 -0300

    ex_modulo2

--- ex_modulo_2.py
+++ ex_modulo_2.py
@@ -19,7 +19,25 @@ def ex3(*args):
         lista_sum.append(item_lista1 + lista2[pos_lista1])
     return lista_sum
 
+def ex4_5():
+    dct_meses_dias = {
+        'janeiro': 31,
+        'fevereiro': 28,
+        'março': 31,
+        'abril': 30,
+        'maio': 31,
+        'junho': 30,
+        'julho': 31,
+        'agosto': 31,
+        'setembro': 30,
+        'outubro': 31,
+        'novembro': 30,
+        'dezembro': 31
+    }
+    for chave in dct_meses_dias:
+        print(chave, '-', dct_meses_dias[chave])
 
-# ex1([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12])
-#ex2()
+ex1([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12])
+ex2()
 print(ex3([1, 2, 3], [4, 5, 6]))
+ex4_5()
